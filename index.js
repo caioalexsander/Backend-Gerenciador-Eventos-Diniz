601f0592c56200faef4b057c256dc11321dc97ef
Caio Alexsander <caio300998@gmail.com>
2026-05-04 23:29:50 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,272 @@
+const express = require('express');
+const cors = require('cors');
+const PDFDocument = require('pdfkit');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/gerar-pdf', (req, res) => {
+  console.log('REQUISIÇÃO RECEBIDA');
+
+  const dados = req.body;
+
+  try {
+    const doc = new PDFDocument();
+
+    function adicionarMarcaDagua(doc) {
+    const pageWidth = doc.page.width;
+    const pageHeight = doc.page.height;
+
+    doc.opacity(0.1); // transparência (0.1 = bem suave)
+
+    doc.image('logo.png',
+        pageWidth / 2 - 250, // centraliza horizontal
+        50, // centraliza vertical
+        { width: 500 }
+    );
+
+    doc.opacity(0.2); // transparência (0.1 = bem suave)
+    
+    doc.image('logo2.png',
+        pageWidth / 2 - 75, // centraliza horizontal
+        pageHeight / 2 - 430, // centraliza vertical
+        { width: 150 }
+    );
+
+    doc.image('logo3.png',
+        pageWidth / 2 - 75, // centraliza horizontal
+        pageHeight - 120,
+        { width: 150 }
+    );
+
+    doc.opacity(1); // volta ao normal
+    
+    }
+
+    // primeira página
+    adicionarMarcaDagua(doc);
+
+    // próximas páginas
+    doc.on('pageAdded', () => {
+    adicionarMarcaDagua(doc);
+    });
+
+    let buffers = [];
+    doc.on('data', buffers.push.bind(buffers));
+
+    doc.on('end', () => {
+      const pdfBase64 = Buffer.concat(buffers).toString('base64');
+      res.json({ pdf: pdfBase64 });
+    });
+
+    doc.page.margins = { top: 50, bottom: 50, left: 50, right: 50 };
+
+doc
+  .fontSize(16)
+  .text('CONTRATO DE PRESTAÇÃO DE SERVIÇOS DE BUFFET', {
+    align: 'center'
+  });
+
+doc.moveDown();
+
+doc.fontSize(12);
+
+doc.font('Helvetica').text(
+  'Pelo presente instrumento particular de prestação de serviços, de um lado Eventos Diniz, com sede na cidade de Belo Horizonte, Estado de Minas Gerais, à Rua São João, nº 83, inscrita no CNPJ sob o nº 28.295.648/0001-51, neste ato representada por ',
+  { continued: true }
+);
+
+doc.font('Helvetica-Bold').text(
+  'Priscila Diniz Delesporte',
+  { continued: true }
+);
+
+doc.font('Helvetica').text(
+  ' ora em diante denominada CONTRATADA, têm entre si como justo e contratado o que segue:'
+);
+
+doc.moveDown();
+
+doc.font('Helvetica').text(
+  '1. A CONTRATADA compromete-se a fornecer todos os serviços de atendimento aos convidados da contratante ',
+  { continued: true }
+);
+
+doc.font('Helvetica-Bold').text(
+  `${dados.nome_contratante || 'CLIENTE'}`,
+  { continued: true }
+);
+
+doc.font('Helvetica').text(
+  ', inscrita pelo CPF ',
+  { continued: true }
+);
+
+doc.font('Helvetica-Bold').text(
+  `${dados.cpf_contratante || 'Não informado'}`,
+  { continued: true }
+);
+
+doc.font('Helvetica').text(
+  ', residente em ',
+  { continued: true }
+);
+
+doc.font('Helvetica-Bold').text(
+  `${dados.residencia_contratante || 'Não informado'}.`,
+);
+
+doc.moveDown();
+
+doc.font('Helvetica').text(
+  '1.1  Esses serviços serão prestados no dia ',
+  { 
+    indent: 20,
+    continued: true 
+  }
+);
+
+doc.font('Helvetica-Bold').text(
+  `${dados.data_evento || 'Não informado'}`,
+  { continued: true }
+);
+
+doc.font('Helvetica').text(
+  ' as ',
+  { continued: true }
+);
+
+doc.font('Helvetica-Bold').text(
+  `${dados.hora_inicio || 'Não informado'}`,
+  { continued: true }
+);
+
+doc.font('Helvetica').text(
+  'ate ',
+  { continued: true }
+);
+
+doc.font('Helvetica-Bold').text(
+  `${dados.hora_fim || 'Não informado'}`,
+  { continued: true }
+);
+
+doc.font('Helvetica').text(
+  ' com duraçao de ',
+  { continued: true }
+);
+
+doc.font('Helvetica-Bold').text(
+  `${dados.duracao || 'Não informado'}`,
+  { continued: true }
+);
+
+doc.font('Helvetica').text(
+  ' horas, no endereço: ',
+  { continued: true }
+);
+
+doc.font('Helvetica-Bold').text(
+  `${dados.local_evento || 'Não informado'} .`
+);
+
+doc.moveDown();
+
+doc.font('Helvetica').text(
+  '1.2  Compreendem a execução do serviço o fornecimento de alimentação, constante de cardápio pré programado entre as partes, foi definido antecipadamente, constando de :',
+  { 
+    indent: 20,
+    continued: true 
+  }
+);
+
+doc.moveDown();
+
+doc.font('Helvetica-Bold').text(
+  `${dados.tipo_evento}.`
+);
+
+doc.moveDown();
+
+doc.font('Helvetica-Bold');
+
+if (dados.cardapio_selecionado?.length) {
+  dados.cardapio_selecionado.forEach(item => {
+    doc.text(`• ${item}`, {
+      indent: 20
+    });
+  });
+} else {
+  doc.text('Nenhum item selecionado', {
+    indent: 20
+  });
+}
+
+doc.font('Helvetica'); // volta ao normal
+
+doc.moveDown();
+
+doc.text(`              
+
+2.  Os serviços ora contratados deverão ser executados por funcionários devidamente habilitados da CONTRATADA, que tem a exclusiva responsabilidade pela sua contratação e demissão, pelo pagamento de seu trabalho, bem como pelo cumprimento de todas as obrigações legais,  de qualquer natureza, para com os mesmos, notadamente as referentes às leis trabalhistas e previdenciárias, ficando dessa forma, expressamente, excluída a responsabilidade da CONTRATANTE sobre tal matéria.                   
+
+3.  A CONTRATADA ficará responsável, ainda, pelas obrigações tributárias decorrentes da prestação dos serviços ora contratados.                   
+
+4.  A CONTRATADA responsabilizar-se-á por todo e qualquer prejuízo que possa ser acarretado à CONTRATANTE em função do descumprimento de dispositivos legais relativos aos serviços acima enumerados.                   
+
+5.  A CONTRATANTE, por sua vez, se obriga a fornecer à CONTRATADA todas as diretrizes dos trabalhos a serem executados.      
+    
+    `, {
+  align: 'justify',
+  width: 450
+});
+
+doc.moveDown();
+
+doc.font('Helvetica').text(
+  '6. ',
+  { continued: true }
+);
+
+doc.moveDown();
+
+doc.font('Helvetica').text(
+  '7. ',
+  { continued: true }
+);
+
+doc.moveDown();
+
+doc.text(`              
+
+8. E de responsabilidade da contratante informar condições do espaço e se possível uma visita técnica do local ate 15 dias antes do evento, o buffet não transporta gás de cozinha,tina,frezzer e gelo.   
+
+9.  Buffet será servido com comidas e bebidas durante 4hs de evento a vontade, se a contratante optar por horas a mais é necessário avisar a coordenadora do evento com 20 minutos antes do término para combinar valor excedente, quebras de tacas por convidados e consume será cobrado uma taxa  por cada peça, taças 8.00 unidade, prato jantar 18.00,prato sobremesa 15.00 consume 13.00 ou o cliente pode comprar a reposição  todas as sobras cruas e congeladas, refrigerantes fechados e sucos pertencem ao buffet, exceto doces, bolo e. Personalizados, quando os mesmo estiverem no contrato, tudo que estiver pronto deixamos para o contratante desde que tenham Vasilhas para colocar, informar ao buffet o número de pessoas de equipe para lanchar . 
+
+    9.1  Responderá por perdas e danos a serem apurados em ação própria a parte que infringir qualquer das cláusulas do presente contrato, casa haja desistência da contratante ou da contratada o valor terá que ser restituído de imediato a parte prejudicada, sendo que desistência gera uma multa de 50% do valor fechado do contrato, a Lista de convidados terá que ser entregue a Eventos Diniz .
+
+10. Fica eleito o Foro da Comarca em Minas Gerais fórum Lafaiete para dirimir qualquer litígio que possa surgir na efetivação do presente contrato.     
+
+    10.1  Cerimonial entra em contato quando fechado conosco, e monta o grupo para homenagens. 
+
+E por estarem as partes de pleno acordo com o disposto neste instrumento particular, assinam-no na presença das duas testemunhas abaixo, em ..... vias de igual teor e forma.
+
+    `, {
+  align: 'justify',
+  width: 450
+});
+
+doc.moveDown();
+
+doc.end();
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Erro ao gerar PDF');
+  }
+});
+
+app.listen(3001, '0.0.0.0', () => {
+  console.log('🚀 Backend rodando');
+});
